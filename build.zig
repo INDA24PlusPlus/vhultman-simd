a4f8b50ed74536fc1761ff55f065ddbd0e4162ff
vidar-h <electsmail@gmail.com>
2025-01-23 23:41:14 +0100

    Update build.zig to new zig master

--- build.zig
+++ build.zig
@@ -1,17 +1,25 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
+    var target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const scalar = b.option(bool, "scalar", "Build the scalar version of the program") orelse true;
-
+    const disable_sse = b.option(bool, "disable-sse", "Disables SSE instructions") orelse false;
     const path = if (scalar) b.path("src/scalar.zig") else b.path("src/simd.zig");
 
-    const exe = b.addExecutable(.{
-        .name = "ray_tracer",
+    if (disable_sse) {
+        target.query.cpu_features_sub.addFeature(@intFromEnum(std.Target.x86.Feature.sse));
+    }
+
+    const exe_mod = b.createModule(.{
         .root_source_file = path,
         .target = target,
         .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "ray_tracer",
+        .root_module = exe_mod,
     });
 
     b.installArtifact(exe);
